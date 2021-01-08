d49def1aa13541355ab57e9e6d76e1f1bc55c2a5
Federico <federico.scarpati1989@gmail.com>
2021-01-08 17:11:35 +0100

    Esercizio completato
    Generate le 10 mail tramite api e stampate in una lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,17 @@
 let app = new Vue({
   el: "#root",
   data: {
-  
+    emails: [
+      
+    ]
+  },
 
-  }
-  
+  mounted(){
+    for (let index = 0; index < 10; index++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then(response => {
+       this.emails.push(response.data.response);
+      });
+    }
+   }
 });
